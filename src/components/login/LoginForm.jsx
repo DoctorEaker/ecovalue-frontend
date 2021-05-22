a6e9e183cccd7d1f5a854336d597829dbb0b33c4
ManuelG28 <manuelalejandrogutierrezmejia@gmail.com>
2021-05-22 15:15:39 -0500

    fet: make login responsive in mobile phones and tablet mobile

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useTranslation } from 'next-i18next';
 import styles from './LoginForm.module.scss'
 import Image from 'next/image';
-import { useForm } from "react-hook-form";
+import { TextField } from '@material-ui/core';
+import Button from '@/components/base/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 
 function LoginForm() {
     const { t } = useTranslation('login');
     return (
         <div className={styles.loginForm}>
-            <div className={styles.loginForm__form}>
+            <div className={styles.loginForm__formContainer}>
                 <div className={styles.loginForm__logo}>
                     <Image
                         src='/images/logo.png'
@@ -21,7 +24,7 @@ function LoginForm() {
                 </div>
                 <div className={styles.loginForm__textContainer}>
                     <h1 className={styles.loginForm__title}>
-                        {t('title')+' '}
+                        {t('title') + ' '}
                         <span className={styles.loginForm__company}>
                             ECOVALUE
                         </span>
@@ -30,12 +33,22 @@ function LoginForm() {
                         {t('subtitle')}
                     </p>
                 </div>
-                <form className={styles.loginForm__inputs}>
-                    <input>
-                    </input>
-                    <input>
-                    </input>
+                <form className={styles.loginForm__form}>
+                    <TextField
+                        label={t('place_holder_1')}
+                        variant="outlined"
+                    />
+                    <TextField
+                        label={t('place_holder_2')}
+                        variant="outlined"
+                    />
+                    <Button>
+                        {t('button')}
+                    </Button>
                 </form>
+                <a className={styles.loginForm__forgotPassword}>
+                    {t('forgot_password')}
+                </a>
             </div>
             <div className={styles.loginForm__image}>
                 <Image
@@ -44,6 +57,15 @@ function LoginForm() {
                     height='1200'
                     width='1200'
                 />
+                <div className={styles.loginForm__imageAlt}>
+                    <FontAwesomeIcon color="white" icon={faChartLine} size="4x" />
+                    <p className={styles.loginForm__altDescription}>
+                        {t('image_alt')}
+                    </p>
+                    <Button>
+                        {t('learn_more')}
+                    </Button>
+                </div>
             </div>
         </div>
     );
